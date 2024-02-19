190ed01e0f0149e71f31a38082070b91f3fdc348
Niall BURDON <niall.burdon@rubrash.com>
2024-02-19 13:57:20 +0100

    base2

---
+++ app.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <!-- LANDING -->
+    <section class="bg-black p-11 h-lvh text-zinc-50">
+      <div>
+        <h1 class="text-9xl font-semibold">I'm Niall Portfolio/24</h1>
+      </div>
+      <nav>
+        <ul class="menu">
+          <li>Projects</li>
+          <li>Contacts</li>
+        </ul>
+      </nav>
+      <div class="container">
+        <div>
+          Product Designer LAST UPDATE ✦ 2023 \/// ©ALEJANDRO ZARZALEJO
+          ((SCROLL))
+        </div>
+        <div>✺Here you can see some of my latest designs and projects</div>
+      </div>
+    </section>
+    <!-- INTRODUCTION TO ME -->
+    <section class="bg-slate-700 p-11 h-lvh">
+      <h1>Crafting nice and useful things for people</h1>
+      <p>
+        I AM CURRENTLY PART OF THE DESIGN TEAM AT EGGS DESIGN﻿ IN MADRID, AND I
+        HAVE HAD THE OPPORTUNITY TO HELP CREATE DIGITAL EXPERIENCES AND
+        SOLUTIONS FOR BIG BRANDS FOR THE LAST 20 YEARS OF MY LIFE. I CONSIDER
+        MYSELF PASSIONATE ABOUT TECHNOLOGY, WORKING AT THE INTERSECTION BETWEEN
+        BRANDING AND DIGITAL PRODUCT DESIGN. NOW, I WORK HAND IN HAND WITH BBVA
+        DESIGN AND USER EXPERIENCE TEAM HELPING TO IMPROVE THEIR DIGITAL
+        PRODUCTS BY BALANCING BUSINESS PRIORITIES WITH THOSE OF THE THOUSANDS OF
+        USERS WHO USE THEIR SERVICES. FROM THE EARLY STAGES OF DEFINITION AND
+        IDEATION, TO THE REVIEW OF INTERACTION FLOWS AND USER TESTING.
+        PREVIOUSLY I HAD THE OPPORTUNITY TO COLLABORATE WITH REPSOL'S DESIGN
+        TEAM, FROM UX TO VISUAL PROCESSES, HELPING LAUNCH FUNCTIONALITIES
+        FOCUSED WHERE REPSOL CURRENTLY OPERATES.
+      </p>
+      <img src="" alt="" />
+    </section>
+    <!-- WHAT I DO -->
+    <section class="bg-slate-400 p-11 h-lvh">
+      <div>
+        <h3>
+          WAYS I CAN HELP YOU AND MORE, AND THINGS I LIKE. LITTLE BY LITTLE I
+          WILL LEARN MORE, EVEN VR, AR, MOTION... I WILL UPDATE IT.
+        </h3>
+        <span>Product design</span>
+        <span>User experience</span>
+        <span>UI design</span>
+        <span>Visual design</span>
+      </div>
+    </section>
+    <!-- RECENT WORK -->
+    <section class="bg-slate-300 p-11 h-lvh">
+      <h1>Recent work</h1>
+      <h3>
+        THESE ARE SOME OF THE PROJECTS THAT I HAVE DONE IN RECENT YEARS,
+        ALTHOUGH I CANNOT SHOW THE MOST RECENT ONES DUE TO CONFIDENTIALITY.
+      </h3>
+      <div>
+        <div id="project"></div>
+        <div id="project name"></div>
+      </div>
+    </section>
+    <footer class="bg-slate-200 p-11 h-lvh">
+      <section>
+        <h1>Tell me something cool or just troll me.</h1>
+        <div id="contactMe">
+          <ul>
+            <li>instagram</li>
+            <li>Linktree</li>
+            <li>email</li>
+          </ul>
+        </div>
+      </section>
+    </footer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li>Projects</li>
+          <li>Contacts</li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
